e565bf1af14e2b6d6264eaca8a38aec4b0a79832
iYefeng <yefeng38083120@126.com>
2016-02-26 00:14:31 +0800

    add files

---
+++ java_cpp/src/javacpp/src/main/java/com/traits/Sample1.java
@@ -0,0 +1,17 @@
+package com.traits;
+
+public class Sample1 
+{
+  public native int intMethod(int n);
+
+  public static void main(String[] args)
+  {
+    System.loadLibrary("Sample1");
+    Sample1 sample = new Sample1();
+    int square = sample.intMethod(5);
+
+    System.out.println("intMethod: " + square);
+  }
+}
+
+
