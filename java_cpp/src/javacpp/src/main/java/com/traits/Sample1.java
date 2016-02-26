221b03358eeb1df9c492941c5a9448e75f885ce8
iYefeng <yefeng38083120@126.com>
2016-02-26 23:58:16 +0800

    add files

--- java_cpp/src/javacpp/src/main/java/com/traits/Sample1.java
+++ java_cpp/src/javacpp/src/main/java/com/traits/Sample1.java
@@ -6,6 +6,7 @@ public class Sample1
 
   public static void main(String[] args)
   {
+    System.out.println(System.getProperty("java.library.path"));
     System.loadLibrary("Sample1");
     Sample1 sample = new Sample1();
     int square = sample.intMethod(5);
