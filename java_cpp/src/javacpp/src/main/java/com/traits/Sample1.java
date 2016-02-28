9b0a67443665cac6108a4c780b3a887898daf07f
iYefeng <yefeng38083120@126.com>
2016-02-28 17:21:08 +0800

    changs

--- java_cpp/src/javacpp/src/main/java/com/traits/Sample1.java
+++ java_cpp/src/javacpp/src/main/java/com/traits/Sample1.java
@@ -1,19 +1,31 @@
 package com.traits;
-import com.traits.Data
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.traits.Data.*;
 
 public class Sample1 
 {
   public native int intMethod(int n
 );
-  public native String[] pbMethod(String[] pb);
+  public native byte[] pbMethod(byte[] pb);
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) throws InvalidProtocolBufferException {
     System.loadLibrary("Sample1");
     Sample1 sample = new Sample1();
     int square = sample.intMethod(5);
-
     System.out.println("intMethod: " + square);
+
+    // protobuf exchange between java and cpp
+    Query.Builder query_builder = Query.newBuilder();
+    query_builder.setNum(123);
+    query_builder.setData("abcdef");
+    Query query = query_builder.build();
+    byte[] query_byte = query.toByteArray();
+
+    byte[] ret_buf = sample.pbMethod(query_byte);
+    Result result = Result.parseFrom(ret_buf);
+    System.out.println("pbMethod: num: " + result.getNum());
+    System.out.println("pbMethod: data: " + result.getData());
   }
 }
 
