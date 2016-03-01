2dd51df400658ee0e41a2bc11966ebc189738332
iYefeng <yefeng38083120@126.com>
2016-03-02 01:12:31 +0800

    add files

---
+++ algorithm/trie/test.cc
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+#include "trie.h"
+
+
+int main(int argc, char *argv[])
+{
+  trie t;
+  t.printTrie(t.root);
+
+  t.insert("你好");
+  t.printTrie(t.root);
+  printf("\n");
+  printf("%ld\n", t.sizeAll(t.root));
+  printf("%ld\n", t.sizeNoneRedundant(t.root));
+  t.erase("你好");
+  t.printTrie(t.root);
+  printf("\n");
+  printf("%ld\n", t.sizeAll(t.root));
+  printf("%ld\n", t.sizeNoneRedundant(t.root));
+  
+  
+  
+  t.insert("你好");
+  t.insert("你好吗");
+  t.insert("你好嗨");
+  t.insert("你好嗨");
+  t.insert("你好坏");
+  t.insert("你好坏");
+  t.insert("你");
+  
+  printf("%ld\n", t.sizeAll(t.root));
+  printf("%ld\n", t.sizeNoneRedundant(t.root));
+
+  t.printTrie(t.root);
+  
+  t.erase("你好");
+  t.erase("你好");
+  t.erase("你好");
+  t.erase("你好");
+  t.erase("你");
+  t.erase("你");
+  t.erase("你好坏");
+  t.erase("你好坏");
+  t.erase("你好吗");
+  t.erase("你好嗨");
+  t.erase("你好嗨");
+  
+  
+  printf("%ld\n", t.sizeAll(t.root));
+  printf("%ld\n", t.sizeNoneRedundant(t.root));
+  t.printTrie(t.root);
+  return 0;
+}
--- algorithm/trie/trie.h
+++ algorithm/trie/trie.h
@@ -5,66 +5,170 @@
 #include <tr1/memory>
 
 using namespace std;
-using namespace std::tr1::shared_ptr;
+using namespace std::tr1;
 
 class trieNode {
   public:
     trieNode() : terminableSize(0), nodeSize(0) {}
-    map<long, shared_ptr<trieNode>> next;
-    
-  private:
+    ~trieNode() {
+      map<long, trieNode*>::iterator iter;
+      for (iter = next.begin(); iter != next.end(); ++iter) {
+        delete iter->second;
+      }
+    }
+    map<long, trieNode*> next;
+
     long terminableSize;
     long nodeSize;
-}
+};
 
 class trie {
   public:
     typedef trieNode Node;
-    typedef shared_ptr<trieNode> pNode;
+    typedef trieNode* pNode;
 
-    trie() : root(new Node) {}
+    trie() : root(new Node) {
+      setlocale(LC_ALL, "");
+    }
+    ~trie() {
+      delete root;
+    }
 
-    void insert(const wchar_t *wc);
-    bool find(const wchar_t *wc);
-    bool erase(const wchar_t *wc);
+    void insert(const char *str);
+    bool find(const char *str);
+    bool downNodeAlone(pNode node);
+    bool erase(const char *str);
     long sizeAll(pNode node);
     long sizeNoneRedundant(pNode node);
+    void printTrie(pNode node);
 
-  private:
     pNode root;
-}
+};
 
-void trie::insert(const wchar *wc)
+void trie::insert(const char *str)
 {
+  wchar_t wc[1024] = {'\0'};
   pNode cur = root;
   pNode pre;
+  mbstowcs(wc, str, strlen(str));
   int len = wcslen(wc);
   int i;
   long key;
   map<long, pNode>::iterator iter;
   for ( i = 0; i < len; ++i) {
     key = (long) wc[i];
-    iter = cur->next.find(wc[i])
+    iter = cur->next.find(key);
     if (iter == cur->next.end()) {
-      cur->next.insert(key, new Node);
+      cur->next.insert(pair<long, pNode>(key, new Node));
       ++cur->nodeSize;
     }
     pre = cur;
-    iter = cur->next.find(key)
-    cur = iter->secode;
+    iter = cur->next.find(key);
+    cur = iter->second;
   }
   ++pre->terminableSize;
 }
 
-bool trie::find(const wchar_t *wc)
+bool trie::find(const char *str)
 {
+  wchar_t wc[1024] = {'\0'};
   pNode cur = root;
   pNode pre;
+  mbstowcs(wc, str, strlen(str));
   int len = wcslen(wc);
   long key;
   map<long, pNode>::iterator iter;
-  for (i 
+  for (int i = 0; i < len; ++i) {
+    key = (long) wc[i];
+    iter = cur->next.find(key);
+    if (iter == cur->next.end()) {
+      return false;
+    }
+    pre = cur;
+    cur = iter->second;
+  }
+  if (pre->terminableSize > 0)
+    return true;
+  return false;
+}
 
+bool trie::downNodeAlone(pNode node)
+{
+  pNode cur = node;
+  int terminableSum = 0;
+  while (cur->nodeSize != 0) {
+    terminableSum += cur->terminableSize;
+    if (cur->nodeSize > 1)
+      return false;
+    else {
+      cur = (cur->next.begin())->second;
+    }
+  }
+  if (terminableSum == 1)
+    return true;
+  return false;
+}
+
+bool trie::erase(const char *str) 
+{
+  wchar_t wc[1024] = {'\0'};
+  mbstowcs(wc, str, strlen(str));
+  int len = wcslen(wc);
+  if (find(str)) {
+    printf("erase %s\n", str);
+    pNode cur = root;
+    pNode pre;
+    map<long, pNode>::iterator iter;
+    int len = wcslen(wc);
+    for (int i = 0; i < len; ++i) {
+      if (downNodeAlone(cur)) {
+        printf("delete %s\n", str);
+        delete cur;
+        return true;
+      }
+      iter = cur->next.find(wc[i]);
+      pre = cur;
+      cur = iter->second;
+    }
+    if (pre->terminableSize > 0) 
+      --pre->terminableSize;
+    return true;
+  }
+  return false;
+}
+
+long trie::sizeAll(pNode node)
+{
+  long rev = node->terminableSize;
+  map<long, pNode>::iterator iter;
+  for (iter = (node->next).begin(); \
+        iter != (node->next).end(); ++iter) {
+    rev += sizeAll(iter->second);
+  }
+  return rev;
+}
+
+long trie::sizeNoneRedundant(pNode node)
+{
+  long rev = 0;
+  if (node->terminableSize > 0)
+    rev = 1;
+  map<long, pNode>::iterator iter;
+  for (iter = (node->next).begin(); \
+        iter != (node->next).end(); ++iter) {
+    rev += sizeNoneRedundant(iter->second);
+  }
+  return rev;
+}
+
+void trie::printTrie(pNode node)
+{
+  map<long, pNode>::iterator iter;
+  for (iter = (node->next).begin(); \
+        iter != (node->next).end(); ++iter) {
+    printf("%C", (wchar_t)iter->first);
+    printTrie(iter->second);
+  }
 }
 
 #endif
