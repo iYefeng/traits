c162d87b4d39844d1ebc178a69a8ac88f8acc0d4
iYefeng <yefeng38083120@126.com>
2016-04-02 02:59:27 +0800

    add file

--- python/web-demo/handler/api/Test.py
+++ python/web-demo/handler/api/Test.py
@@ -5,7 +5,11 @@ import tornado.web
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+import logging
+
+logger = logging.getLogger("tornado.access")
 
 class TestHandler(tornado.web.RequestHandler):
   def get(self):
+    logger.info("haha it is working")
     self.write("hello world")
