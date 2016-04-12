4403efef41567724e0858eaed11fdaa29e9985de
iYefeng <yefeng38083120@126.com>
2016-04-13 01:35:27 +0800

    add file

---
+++ python/web-demo/static/app/app.js
@@ -0,0 +1,30 @@
+angular.module("myApp", ["ngTable", "ngResource"]);
+
+(function() {
+  "use strict";
+
+  angular.module("myApp").controller("demoController", function ($scope, $http, NgTableParams) {
+    var self = this;
+    
+    $http.get("data1.txt")
+    .success(function(response) {
+        self.tableParams = new NgTableParams({
+            sorting: { title: "asc" }
+        }, {
+          dataset: response
+        });
+    });
+  });
+})();
+
+(function() {
+  "use strict";
+
+  angular.module("myApp").run(setRunPhaseDefaults);
+  setRunPhaseDefaults.$inject = ["ngTableDefaults"];
+
+  function setRunPhaseDefaults(ngTableDefaults) {
+    ngTableDefaults.params.count = 10;
+    ngTableDefaults.settings.counts = [];
+  }
+})();
