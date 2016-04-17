0d97892db9a055fdca36c6012e46e1468c3b6e1b
iYefeng <yefeng38083120@126.com>
2016-04-17 20:23:29 +0800

    add files

--- algorithm/ac/src/ac.h
+++ algorithm/ac/src/ac.h
@@ -1,34 +1,81 @@
 #ifndef __AC_H__
 #define __AC_H__
 
+#include <vector>
+#include <set>
 #include <map>
 #include <tr1/memory>
+#include <string>
 
 using namespace std;
 using namespace std::tr1;
 
-class acNode {
+class AcNode {
   public:
-    acNode(): terminableSize(0), nodeSize(0) {}
-    ~acNode() {
-      for (map<long, acNode*>::iterator iter = next.begin(); \
-            iter != next.end(); ++iter) {
+    AcNode(): terminableSize_(0), nodeSize_(0), patternTag_(0), \
+              parent_(NULL), fail_(NULL) {}
+    ~AcNode() {
+      for (map<long, AcNode*>::iterator iter = next_.begin(); \
+            iter != next_.end(); ++iter) {
         delete iter->second;
       }
     }
 
-    int patternTag;
-    int patternNo;
-    int terminableSize;
-    int nodeSize;
-    acNode* parent;
-    acNode* fail;
-    map<long, acNode*> next;
+    int patternTag_;
+    int patternNo_;
+    int terminableSize_;
+    int nodeSize_;
+    AcNode* parent_;
+    AcNode* fail_;
+    map<long, AcNode*> next_;
 };
 
-class ac {
+class AC {
+  public:
+    typedef AcNode Node;
+    typedef AcNode* pNode;
+    typedef struct result {
+      string pattern;
+      int location;
+    } acResult;
+    
+    AC(): root_(new Node), patternCnt_(0) {
+      setlocale(LC_ALL, "");
+    }
+    ~AC() {
+      delete root_;
+    }
+  
+    int loadPattern(const vector<string> vStr);
+    int buildTree();
+    int insert(const char *str);
+    int buildFailPath();
+    int match(const string doc);
 
+    pNode root_;
+    map<int, string> patternIdx_;
+    int patternCnt_;
+    vector<acResult> result_;
 };
+
+int AC::loadPattern(const vector<string> vStr)
+{
+  int size_t len = vStr.size();
+  for (int i = 0; i < len; ++i) {
+    patternIdx_.insert(pair<int, string>(patternCnt_, vStr[i]));
+    ++patternCnt_;
+  }
+  return 0;  
+}
+
+int AC::buildTree()
+{
+  pNode tmp1 = NULL, tmp2 = NULL;
+  for (map<int, string>::iterator iter = patternIdx_.begin(); iter != patternIdx_.end(); ++iter) {
+    tmp1 = root;
+
+  }
+}
 
 
 #endif // __AC_H__
