c1377fd43faff00eece1d3693c70e47cd7f51a0b
iYefeng <yefeng38083120@126.com>
2016-05-02 01:16:38 +0800

    add file

--- python/zookeeper/main.py
+++ python/zookeeper/main.py
@@ -16,13 +16,13 @@ log = logging
 class ZookeeperStore(object):
   
   ZK_HOST = "127.0.0.1:32181"
-  ROOT = "/webSpider"
-  WORKERS_PATH = os.path.join(ROOT, "workers")
   TIMEOUT = 1000
 
-  def __init__(self, verbose = True):
+  def __init__(self, verbose = True, database='/test', table='targets'):
     self.VERBOSE = verbose
     self.path = None
+    self.database = database
+    self.table = os.path.join(self.database, table)
 
     self.zk = ZKClient(self.ZK_HOST, timeout = self.TIMEOUT)
     self.say("login ok")
@@ -32,7 +32,7 @@ class ZookeeperStore(object):
     self.zk.close()
 
   def _init_zk(self):
-    nodes = (self.ROOT, self.WORKERS_PATH)
+    nodes = (self.database, self.table)
     for node in nodes:
       if not self.zk.exists(node):
         try:
@@ -40,14 +40,29 @@ class ZookeeperStore(object):
         except:
           pass
 
-  def create(self, msg):
+  def create(self, path, msg, flags=0):
     msg = self.encode(msg)
-    self.path = self.zk.create(self.WORKERS_PATH + "/worker", msg, flags=zookeeper.EPHEMERAL | zookeeper.SEQUENCE)
-    self.path = os.path.basename(self.path)
+    self.path = os.path.join(self.table, path)
+    nodes = self.path.split("/")
+    # FIXME
+    for i in range(4,len(nodes)):
+      node = "/".join(nodes[:i])
+      if not self.zk.exists(node):
+        try:
+          self.zk.create(node, "")
+        except:
+          pass
+    if not self.zk.exists(self.path):
+      self.path = self.zk.create(self.path, msg, flags=flags)
+    else:
+      self.zk.set(self.path, msg)
+    #self.path = os.path.basename(self.path)
     self.say("register ok! I'm %s" % self.path)
   
-  def get_children(self):
-    children = self.zk.get_children(self.WORKERS_PATH)
+  def get_children(self, path=""):
+    if not path:
+      path = self.table
+    children = self.zk.get_children(path)
     return children
 
   def get(self, node):
@@ -70,15 +85,30 @@ class ZookeeperStore(object):
 class JsonZookeeperStore(ZookeeperStore):
 
   def decode(self, msg):
-    return json.loads(msg)
+    if msg:
+      return json.loads(msg)
+    else:
+      return {}
   
   def encode(self, obj):
-    return json.dumps(obj)
+    if obj:
+      return json.dumps(obj)
+    else:
+      return ""
 
 if __name__ == "__main__":
-  zks = JsonZookeeperStore()
-  zks.create({"url":"127.0.0.1:9000"})
-  zks.create({"url":"127.0.0.1:9001"})
+  zks = JsonZookeeperStore(database="/webSpider", table="targets")
+  zks.create("127.0.0.1:9000", {"url":"127.0.0.1:9000"}, 1)
+  zks.create("127.0.0.1:9001", {"url":"127.0.0.1:9001"}, 1)
   children = zks.get_children()
   for child in children:
-    print zks.decode(zks.get(zks.WORKERS_PATH + "/" + child)[0])["url"]
+    print zks.decode(zks.get(zks.table + "/" + child)[0])["url"]
+
+  zkeggs = JsonZookeeperStore(database="/webSpider", table="eggs")
+  zkeggs.create("project1/v1.0", {"data":"hello1"}, 0)
+  zkeggs.create("project1/v2.0", {"data":"hello2"}, 0)
+  zkeggs.create("project2/v2.0", {"data":"hello2"}, 0)
+  children = zkeggs.get_children()
+  for child in children:
+    data = zkeggs.get_children(zkeggs.table + "/" + child)
+    print data
