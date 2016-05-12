e7a48dc1425d0f883fa6fb2facd3238a9fa357bc
iYefeng <yefeng38083120@126.com>
2016-05-13 01:58:41 +0800

    add files java_webb

---
+++ java_web/server/src/main/java/com/traits/WebAppContextWithWarServer.java
@@ -0,0 +1,23 @@
+package com.traits;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class WebAppContextWithWarServer {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(38080);
+
+        WebAppContext context = new WebAppContext();
+        String root = System.getProperty("user.dir");
+        System.out.println(root);
+        context.setContextPath("/myapp");
+        context.setWar("scheduler/target/scheduler-1.0-SNAPSHOT.war");
+        //context.setContextPath("/test");
+        //context.setDescriptor(root + "/scheduler/out/artifacts/scheduler_war_exploded/WEB-INF/web.xml");
+        //context.setResourceBase(root + "/scheduler/out/artifacts/scheduler_war_exploded/");
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
