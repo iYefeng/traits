2d7515b60f7e55657bfbb4b9b1c7b1ffb0941bc1
iYefeng <yefeng38083120@126.com>
2016-06-10 22:18:57 +0800

    add files

--- algorithm/decisiontree/src/decisiontree.py
+++ algorithm/decisiontree/src/decisiontree.py
@@ -2,8 +2,50 @@
 # -*- coding: utf-8 -*-
 
 from math import log
+import pickle
 
 class DecisionTree():
+
+  def __init__(self, name=None, dataSet=None, labels=None, infilename=None, outfilename=None):
+    if infilename:
+      self.isModelExists_ = True
+      self.infilename_ = infilename
+      tree, labels = self.grabTree(infilename)
+      self.outfilename_ = outfilename
+      self.tree_ = tree
+      self.labels_ = labels
+      self.dataSet_ = None
+      self.name_ = name if name else "test"
+    else:
+      self.isModelExists_ = False
+      self.infilename_ = infilename
+      self.outfilename_ = outfilename
+      self.tree_ = None
+      self.labels_ = labels
+      self.dataSet_ = dataSet
+      self.name_ = name if name else "test"
+
+  def __del__(self):
+    if not self.outfilename_:
+      if not self.isModelExists_ and self.name_:
+        filename = "../models/%s.model" % self.name_
+        self.storeTree(filename)
+    else:
+      self.storeTree(self.outfilename_)
+
+  def grabTree(self, filename):
+    fr = open(filename)
+    data = pickle.load(fr)
+    fr.close()
+    return (data["tree"], data["labels"])
+
+  def storeTree(self, filename):
+    fw = open(filename, 'w')
+    data = {}
+    data["tree"] = self.tree_
+    data["labels"] = self.labels_
+    pickle.dump(data, fw)
+    fw.close()
 
   def calcShannonEnt(self, dataSet):
     numEntries = len(dataSet)
@@ -19,9 +61,84 @@ class DecisionTree():
       shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
 
+  def splitDataSet(self, dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+      if featVec[axis] == value:
+        reducedFeatVec = featVec[:axis]
+        reducedFeatVec.extend(featVec[axis+1:])
+        retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+  def chooseBestFeatureToSplit(self, dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = self.calcShannonEnt(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+    for i in range(numFeatures):
+      featList = [example[i] for example in dataSet]
+      uniqueVals = set(featList)
+      newEntropy = 0.0
+      for value in uniqueVals:
+        subDataSet = self.splitDataSet(dataSet, i, value)
+        prob = len(subDataSet)/float(len(dataSet))
+        newEntropy += prob * self.calcShannonEnt(subDataSet)
+      infoGain = baseEntropy - newEntropy
+      if (infoGain > bestInfoGain):
+        bestInfoGain = infoGain
+        bestFeature = i
+    return bestFeature
+  
+  def majorityCnt(self, classList):
+    classCount = {}
+    for vote in classList:
+      if vote not in classCount.keys(): classCount[vote] = 0
+      classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), \
+        key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+  def createTree(self):
+    import copy
+    if self.dataSet_ and self.labels_:
+      tmpLabels = copy.deepcopy(self.labels_)
+      self.tree_ = self._createTree(self.dataSet_, tmpLabels)
+
+  def _createTree(self, dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+      return classList[0]
+    if len(dataSet[0]) == 1:
+      return self.majorityCnt(classList)
+    bestFeat = self.chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+      subLabels = labels[:]
+      myTree[bestFeatLabel][value] = self._createTree(self.splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+  def classify(self, testVec):
+    if self.tree_ and self.labels_:
+      return self._classify(self.tree_, self.labels_, testVec)
+    else:
+      print "please create tree first"
+      return None
+
+  def _classify(self, inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    key = testVec[featIndex]
+    valueOfFeat = secondDict[key]
+    if isinstance(valueOfFeat, dict): 
+      classLabel = self._classify(valueOfFeat, featLabels, testVec)
+    else: classLabel = valueOfFeat
+    return classLabel
 
 if __name__ == "__main__":
-  tree = DecisionTree()
   dataSet = [
       [1, 1, 'yes'],
       [1, 1, 'yes'],
@@ -29,4 +146,16 @@ if __name__ == "__main__":
       [0, 1, 'no'],
       [0, 1, 'no']
       ]
-  print tree.calcShannonEnt(dataSet)
+  labels = ['no surfacing','flippers']
+  tree = DecisionTree("test", dataSet, labels)
+  tree.createTree()
+  print tree.classify([0,0])
+  #print tree.calcShannonEnt(dataSet)
+  #print tree.splitDataSet(dataSet, 0, 1)
+  #print tree.splitDataSet(dataSet, 0, 0)
+  #print tree.chooseBestFeatureToSplit(dataSet)
+  #print tree.createTree(dataSet, labels)
+
+  tree1 = DecisionTree(infilename="../models/test.model")
+  print tree.classify([1,1])
+
