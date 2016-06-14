8596ba86ef9c3dd06faf02b07fa71a9eeffd62c9
iYefeng <yefeng38083120@126.com>
2016-06-15 00:49:49 +0800

    add files

--- algorithm/bayes/src/bayes.py
+++ algorithm/bayes/src/bayes.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class Bayes:
 
+  def createVocabList(self, dataSet):
+    vocabSet = set()
+    for doc in dataSet:
+      vocabSet = vocabSet | set(doc)
+    return list(vocabSet)
+
+  def words2Vec(self, vocabList, inputSet):
+    retVec = [0] * len(vocabList)
+    for word in inputSet:
+      if word in vocabList:
+        retVec[vocabList.index(word)] += 1
+    return retVec
+  
+  def _train(self, trainMaxtrix, trainLabel):
 
 
 if __name__ == "__main__":
