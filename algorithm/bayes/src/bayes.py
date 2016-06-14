aa2b4f74cc222192b552250de149a88debed97ea
iYefeng <yefeng38083120@126.com>
2016-06-15 00:24:46 +0800

    add files

---
+++ algorithm/bayes/src/bayes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+
+if __name__ == "__main__":
+  docs = [
+      ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+      ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+      ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+      ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+      ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+      ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']
+    ]
+  labels = [0,1,0,1,0,1]
+
