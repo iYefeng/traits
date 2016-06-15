95255ee5a6e1063f864c714df056aa3cfdf66991
iYefeng <yefeng38083120@126.com>
2016-06-16 00:39:07 +0800

    add files

--- algorithm/bayes/src/bayes.py
+++ algorithm/bayes/src/bayes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from numpy import *
+
 class Bayes:
 
   def createVocabList(self, dataSet):
@@ -9,14 +11,47 @@ class Bayes:
       vocabSet = vocabSet | set(doc)
     return list(vocabSet)
 
-  def words2Vec(self, vocabList, inputSet):
-    retVec = [0] * len(vocabList)
+  def words2Vec(self, inputSet):
+    retVec = [0] * len(self.vocabList_)
     for word in inputSet:
-      if word in vocabList:
-        retVec[vocabList.index(word)] += 1
+      if word in self.vocabList_:
+        retVec[self.vocabList_.index(word)] += 1
     return retVec
   
   def _train(self, trainMaxtrix, trainLabel):
+    numTrainDocs = len(trainMaxtrix)
+    numWords = len(trainMaxtrix[0])
+    pAbusive = sum(trainLabel) / float(numTrainDocs)
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+    for i in xrange(numTrainDocs):
+      if trainLabel[i] == 1:
+        p1Num += trainMaxtrix[i]
+        p1Denom += sum(trainMaxtrix[i])
+      else:
+        p0Num += trainMaxtrix[i]
+        p0Denom += sum(trainMaxtrix[i])
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+  def train(self, dataSet, labels):
+    self.vocabList_ = self.createVocabList(dataSet)
+    trainMat = []
+    for doc in dataSet:
+      trainMat.append(self.words2Vec(doc))
+    self.p0V_, self.p1V_, self.pAb_ = self._train(array(trainMat), array(labels))
+
+
+  def classify(self, vec2Classify):
+    p1 = sum(vec2Classify * self.p1V_) + log(self.pAb_)
+    p0 = sum(vec2Classify * self.p0V_) + log(1 - self.pAb_)
+    if p1 > p0:
+      return 1
+    else:
+      return 0
 
 
 if __name__ == "__main__":
