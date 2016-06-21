a0a522a0c0b1ab6cb2d6d64b2e0289711535897f
iYefeng <yefeng38083120@126.com>
2016-06-22 00:36:00 +0800

    add files

---
+++ algorithm/randomforest/src/random_forest.h
@@ -0,0 +1,24 @@
+/**
+ *   name: random forest
+ * author: yefeng
+ *   data: 2016-06-22
+ *
+ **/
+#ifndef __RANDOM_FOREST_H__
+#define __RANDOM_FOREST_H__
+
+#include <iostream>
+
+using namespace std;
+
+class RandomForest {
+public:
+  
+
+private:
+  
+
+};
+
+#endif // __RANDOM_FOREST_H__
+
