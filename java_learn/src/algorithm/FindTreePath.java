54a7cbb9dd1b55f9f635dc7092979c1885bf7e32
iYefeng <yefeng38083120@126.com>
2016-11-19 21:09:56 +0800

    add files

--- java_learn/src/algorithm/FindTreePath.java
+++ java_learn/src/algorithm/FindTreePath.java
@@ -2,6 +2,14 @@ package algorithm;
 
 /**
  * Created by yefeng on 16/11/19.
+ * 题目:输入一个整数和一棵二元树。 从树的根结点开始往下访问一直到叶结点所经过的所有结点形成一条路径。 打印出和与输入整数相等的所有路径。
+ * 例如输入整数22和如下二元树
+ *    10
+ *    /\
+ *   5 12
+ *  /\
+ * 4 7
+ * 则打印出两条路径:10, 12 和10, 5, 7。
  */
 public class FindTreePath {
     static public class BTNode {
