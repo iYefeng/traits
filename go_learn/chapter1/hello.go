36fd1885ac817490728535587c7b82e59d406cc5
iYefeng <yefeng38083120@126.com>
2017-01-21 17:33:52 +0800

    add files

---
+++ go_learn/chapter1/hello.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main() {
+  /* This is my first sample program. */
+  fmt.Println("Hello, World!");
+}
---
+++ go_learn/chapter2/lesson1.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main() {
+  var x int;
+  x = 2;
+  fmt.Println(x);
+  fmt.Printf("x is of type %T\n", x);
+  
+  y := 13.0;
+  fmt.Println(y);
+  fmt.Printf("y is of type %T\n", y)
+
+}
