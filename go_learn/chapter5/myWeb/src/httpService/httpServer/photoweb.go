7ebe46a5423dfa6ec4aa8c90dffbd372a073aba3
iYefeng <yefeng38083120@126.com>
2017-03-05 16:57:29 +0800

    add fiels

---
+++ go_learn/chapter5/myWeb/src/httpService/httpServer/photoweb.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	UPLOAD_DIR = "./uploads"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		io.WriteString(w, "<html><body><form method=\"POST\" action=\"/upload\" "+
+			"enctype=\"multipart/form-data\">"+
+			"Choose an image to upload: <input name=\"image\" type=\"file\" />"+
+			"<input type=\"submit\" value=\"Upload\"/>"+
+			"</form></body></html>")
+		return
+	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		filename := h.Filename
+		defer f.Close()
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var listHtml string
+	for _, fileInfo := range fileInfoArr {
+		imgId := fileInfo.Name()
+		listHtml += "<li><a href=\"/view?id=" + imgId + "\">" + imgId + "</a></li>"
+	}
+	io.WriteString(w, "<html><body><ol>"+listHtml+"</ol></body></html>")
+}
+
+func main() {
+	http.HandleFunc("/", listHandler)
+	http.HandleFunc("/view", viewHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenServer:", err.Error())
+	}
+}
